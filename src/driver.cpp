08e3d5165d1d6a35617da696a513d3242e36f9dd
YipengWang1040 <ywang1040@ucr.edu>
2022-10-12 16:11:32 -0700

    temporary commit

--- src/driver.cpp
+++ src/driver.cpp
@@ -2,6 +2,7 @@
 #include <csignal>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
 
 #include <ros/ros.h>
 #include <std_msgs/Header.h>
@@ -17,8 +18,8 @@
 using namespace std;
 
 static int skip=0;
-static string topic_rgb="/blackfly/cam0/image_color";
-static string topic_raw="/blackfly/cam0/image_raw";
+static string topic_rgb="/blackfly/cam%/image_color";
+static string topic_raw="/blackfly/cam%/image_raw";
 static string topic_addition="/blackfly/additional";
 static int64_t delta=-1;
 
@@ -31,45 +32,22 @@ void sigint_handler(int){
     }
 }
 
-void apply_configs(Device& camera, configure::config_reader& reader){
-    cout<<reader.get_camera_configs().size()<<endl;
-    for(auto&& config:reader.get_camera_configs()){
-        if(!camera.configure(config)){
-            ROS_FATAL("Failed to config camera at %s",config.field.c_str());
+void apply_configs(std::list<Device>& cameras, configure::config_reader& reader){
+    for(auto&& camera:cameras){
+        for (auto&& config : reader.get_camera_configs()) {
+            if (!camera.configure(config)) {
+                ROS_FATAL("Failed to config camera at %s", config.field.c_str());
+            }
         }
     }
-
     skip=reader.get("skip",0);
-    topic_raw=reader.get("ros_topic_raw",string("/blackfly/cam0/image_raw"));
-    topic_rgb=reader.get("ros_topic_color",string("/blackfly/cam0/image_color"));
+    topic_raw=reader.get("ros_topic_raw",string("/blackfly/cam%/image_raw"));
+    topic_rgb=reader.get("ros_topic_color",string("/blackfly/cam%/image_color"));
     topic_addition=reader.get("additional_topic",string("/blackfly/additional"));
 }
 
-
-int main(int argc, char* argv[]){
-    ros::init(argc, argv, "flir_driver");
-    ros::NodeHandle nh("~");
-    ros::Publisher pub_image_raw=nh.advertise<sensor_msgs::Image>(topic_raw,10);
-    ros::Publisher pub_image_rgb=nh.advertise<sensor_msgs::Image>(topic_rgb,10);
-    ros::Publisher pub_addition=nh.advertise<flir_camera_driver::ImageAddition>(topic_addition,10);
-
-    signal(SIGINT,sigint_handler);
-
-    Spinnaker::SystemPtr system=Spinnaker::System::GetInstance();
-    auto list=Device::enumerate();
-
-    Device camera=std::move(*list.begin());
-    if(!camera.default_initialization()){
-        ROS_FATAL("Failed to initialze the camera!");
-        return -1;
-    }
-
-    string config_file=nh.param("config_file",string("src/myflir/config/default.yaml"));
-    configure::config_reader reader(config_file);
-    apply_configs(camera,reader);
-
-    ROS_INFO("starting");
-    int counter=0;
+void run_cam(Device& camera,ros::Publisher& pub_raw, ros::Publisher& pub_rgb, ros::Publisher& pub_addition){
+    int counter=skip;
     while(run){
         cv::Mat image_rgb;
         cv::Mat image_raw;
@@ -96,7 +74,7 @@ int main(int argc, char* argv[]){
                 cv_image.header.frame_id="image0";
                 cv_image.encoding="bgr8";
                 cv_image.image=image_rgb;
-                pub_image_rgb.publish(cv_image.toImageMsg());
+                pub_rgb.publish(cv_image.toImageMsg());
             }
 
             if(!image_raw.empty()){
@@ -105,7 +83,7 @@ int main(int argc, char* argv[]){
                 cv_image.header.frame_id="image0";
                 cv_image.encoding="bgr8";
                 cv_image.image=image_raw;
-                pub_image_raw.publish(cv_image.toImageMsg());
+                pub_raw.publish(cv_image.toImageMsg());
             }
 
             if(!image_raw.empty() || !image_rgb.empty()){
@@ -123,8 +101,44 @@ int main(int argc, char* argv[]){
             usleep(100000);
         }
     }
+}
 
-    camera.clear();
+int main(int argc, char* argv[]){
+    ros::init(argc, argv, "flir_driver");
+    ros::NodeHandle nh("~");
+
+    signal(SIGINT,sigint_handler);
+
+    Spinnaker::SystemPtr system=Spinnaker::System::GetInstance();
+    auto camera_list=Device::enumerate();
+
+    for (auto&& camera : camera_list) {
+        if (!camera.default_initialization()) {
+            ROS_FATAL("Failed to initialze the camera!");
+            return -1;
+        }
+    }
+
+    string config_file=nh.param("config_file",string("src/myflir/config/default.yaml"));
+    configure::config_reader reader(config_file);
+    apply_configs(camera_list,reader);
+
+    ROS_INFO("starting");
+
+    // initialize publishers
+    ros::Publisher pub_addition=nh.advertise<flir_camera_driver::ImageAddition>(topic_addition,10);
+    std::vector<ros::Publisher> pubs_image_raw;
+    std::vector<ros::Publisher> pubs_image_rgb;
+    std::vector<std::thread> working_threads;
+    size_t idx=0;
+    for(auto&& camera:camera_list){
+        pubs_image_raw.push_back(nh.advertise<sensor_msgs::Image>(topic_rgb,10));
+    }
+    
+
+    for(auto&& camera:camera_list){
+        camera.clear();
+    }
     system->ReleaseInstance();
     return 0;
 }
